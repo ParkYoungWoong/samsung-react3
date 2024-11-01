ef825bdddb2279a313711812cb816313325ef100
ParkYoungWoong <thesecon@gmail.com>
2024-11-01 16:38:58 +0900

    영화 포스터 URL을 Next.js 원격 패턴으로 등록.

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'heropy.dev'
+      },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com'
       }
     ]
   }
